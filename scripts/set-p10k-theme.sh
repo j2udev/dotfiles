c2bb5780a3c6dce92269ba4b1a944c9baf85bbe0
Joshua Ward <j2udevelopment@gmail.com>
2020-06-14 22:03:44 -0400

    temp commit while I workout why the scripts can't be called from each other within asciinema

---
+++ scripts/set-p10k-theme.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+set -e
+
+P10K_PROFILE=$1
+
+WHITE_ON_GREEN="\033[37;42m"
+WHITE_ON_RED="\033[37;41m"
+END_OF_COLOR="\033[0m"
+
+cd "$(dirname "$BASH_SOURCE")"
+
+# This will backup any existing .p10k.zsh files to the 
+[[ -f ~/.p10k.zsh ]] && mv ~/.p10k.zsh ~/.p10k_backup.zsh
+
+case $P10K_PROFILE in 
+  1 ) 
+    echo -e $WHITE_ON_GREEN"Setting Spring Powerlevel10k Theme..."$END_OF_COLOR;
+    cp ../p10k/spring-p10k.zsh ~/.p10k.zsh
+    ;;
+  2 )
+    echo -e $WHITE_ON_GREEN"Setting WVU Powerlevel10k Theme..."$END_OF_COLOR;
+    cp ../p10k/wvu-p10k.zsh ~/.p10k.zsh
+    ;;
+  * )
+    echo -e $WHITE_ON_GREEN"Unrecognized Option"$END_OF_COLOR;
+    ;;    
+esac
