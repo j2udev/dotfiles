b522404ef2a2bdd8393ada52e85c7e841d326604
Joshua Ward <j2udevelopment@gmail.com>
2020-07-11 20:18:23 -0400

    alias finder plugin wasn't working so I just wrote my own function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,6 @@
+# General
+function af { alias | grep "$*" }
+
 # Powerlevel10k
 function prompt-theme { sh $HOME/dotfiles/scripts/p10k-theme-menu.sh ; source $HOME/.zshrc }
 
@@ -5,4 +8,6 @@ function prompt-theme { sh $HOME/dotfiles/scripts/p10k-theme-menu.sh ; source $H
 function drmig { docker rmi $(docker images | grep $1 | xargs -L2| awk '{print($3)}') }
 function drmigf { docker rmi --force $(docker images | grep $1 | xargs -L2| awk '{print($3)}') }
 function lint { docker run -e RUN_LOCAL=true -v $(pwd):/tmp/lint/ github/super-linter }
+
+
 
