1677671ed581f7d204ec440fd7281c1b028e2034
Joshua Ward <j2udevelopment@gmail.com>
2020-07-21 09:02:19 -0400

    added k8s context merge function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,6 +4,9 @@ function af { alias | grep "$*" }
 # Powerlevel10k
 function prompt-theme { sh $HOME/dotfiles/scripts/p10k-theme-menu.sh ; source $HOME/.zshrc }
 
+# Kubernetes
+function kconm { cp ~/.kube/config ~/.kube/config.backup && KUBECONFIG=~/.kube/config:~/.kube/$1 kubectl config view --flatten > /tmp/config && mv /tmp/config ~/.kube/config }
+
 # Docker
 function drmig { docker rmi $(docker images | grep $1 | xargs -L2| awk '{print($3)}') }
 function drmigf { docker rmi --force $(docker images | grep $1 | xargs -L2| awk '{print($3)}') }
